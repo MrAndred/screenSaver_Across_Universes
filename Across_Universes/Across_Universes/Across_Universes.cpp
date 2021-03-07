b1d37a65d8dc26775857683a5ddade23cd7bd34d
MrAndred <mrandrex1337@gmail.com>
2021-03-07 15:03:19 +0300

    Working_screensaver

---
+++ Across_Universes/Across_Universes/Across_Universes.cpp
@@ -0,0 +1,272 @@
+﻿// Across_Universes.cpp : Определяет точку входа для приложения.
+//
+
+
+#include "framework.h"
+#include "Across_Universes.h"
+
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+
+struct Star  // Структура описывающая точку(звезду) в пространстве
+{
+    float X;
+    float Y;
+    float Z;
+};
+
+const int Speed = 5;           //  Скорость полета точек(звезд)
+const int StarCount = 1000;  //  Кол-во генерируемых точек(звезд)
+int D = 0;
+int oX = 0;
+int oY = 0;
+int ClientWidth;
+int ClientHeight;
+Star star[StarCount];
+PAINTSTRUCT ps;
+HDC hdc;
+HDC compdc;
+HBITMAP buffer;
+
+HWND MainWnd;
+
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+void InitProgram()
+{
+    ClientWidth = GetSystemMetrics(SM_CXSCREEN);
+    ClientHeight = GetSystemMetrics(SM_CYSCREEN);
+
+    D = (ClientWidth + ClientHeight) / 2;
+
+    // Вычисляем центр экрана
+    oX = ClientWidth / 2;
+    oY = ClientHeight / 2;
+
+    // Генерация трехмерных точек
+
+    for (int i = 0; i < StarCount; i++)
+    {
+        star[i].X = (rand() % ClientWidth) - oX;
+        star[i].Y = (rand() % ClientHeight) - oY;
+        star[i].Z = (rand() % ClientWidth) - oX;
+    }
+
+}
+
+void MoveStars()
+{
+    int X, Y;
+    float k, rPoint;
+
+    for (int i = 0; i < StarCount; i++)
+    {
+        star[i].Z = star[i].Z - Speed;
+
+        if (star[i].Z <= -D)  star[i].Z = 0;
+
+        if ((star[i].Z + D) == 0) star[i].Z = 0.1;
+
+        k = D / (star[i].Z + D);
+
+        X = oX + floor(star[i].X * k);
+        Y = oY + floor(star[i].Y * k);
+
+        rPoint = star[i].Z * 0.005;
+
+        Ellipse(compdc, X - rPoint, Y - rPoint, X + rPoint, Y + rPoint);
+
+    }
+
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_ACROSSUNIVERSES, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_ACROSSUNIVERSES));
+
+    SetTimer(MainWnd, 0, 1, 0);
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    KillTimer(MainWnd, 0);
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = 0;
+    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground  = 0;
+    wcex.lpszMenuName   = 0;
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm = 0;
+
+    return RegisterClassEx(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+   InitProgram();
+   MainWnd = CreateWindowEx(
+       WS_EX_TOPMOST,
+       szWindowClass,
+       szWindowClass,
+       WS_POPUP | WS_VISIBLE,
+       0,
+       0,
+       ClientWidth,
+       ClientHeight,
+       NULL,
+       NULL,
+       hInstance,
+       NULL);
+
+   if (!MainWnd)
+   {
+       return FALSE;
+   }
+
+   ShowWindow(MainWnd, nCmdShow);
+   UpdateWindow(MainWnd);
+
+   return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    HPEN hpen;
+    HBRUSH hbrush;
+    switch (message)
+    {
+
+    case WM_CREATE:
+    {
+        hdc = GetDC(hWnd);
+        compdc = CreateCompatibleDC(hdc);
+        buffer = CreateCompatibleBitmap(hdc, ClientWidth, ClientHeight);
+        SelectObject(compdc, buffer);
+        ReleaseDC(hWnd, hdc);
+    }	break;
+
+    case WM_KEYDOWN:
+    {
+        if (wParam == VK_ESCAPE)  SendMessage(hWnd, WM_CLOSE, 0, 0);
+    }	break;
+
+    case WM_TIMER: InvalidateRect(hWnd, 0, FALSE);
+
+    case WM_PAINT:
+    {
+        hdc = BeginPaint(hWnd, &ps);
+
+        // Стираем буфер рисования 
+        hpen = (HPEN)GetStockObject(BLACK_PEN);
+        hbrush = (HBRUSH)GetStockObject(BLACK_BRUSH);
+        SelectObject(compdc, hpen);
+        SelectObject(compdc, hbrush);
+        Rectangle(compdc, 0, 0, ClientWidth, ClientHeight);
+
+        // Рисуем звезды белым цветом 
+        hpen = (HPEN)GetStockObject(WHITE_PEN);
+        hbrush = (HBRUSH)GetStockObject(WHITE_BRUSH);
+        SelectObject(compdc, hpen);
+        SelectObject(compdc, hbrush);
+        MoveStars();
+
+        // Скидываем буфер рисования на экран 
+        BitBlt(hdc, 0, 0, ClientWidth, ClientHeight, compdc, 0, 0, SRCCOPY);
+
+        EndPaint(hWnd, &ps);
+    }	break;
+
+    case WM_DESTROY:
+    {
+        DeleteObject(compdc);
+        DeleteObject(buffer);
+        PostQuitMessage(0);
+    }	break;
+
+    default: return DefWindowProc(hWnd, message, wParam, lParam);
+
+    }
+
+    return 0;
+}
+
+
---
+++ Across_Universes/Across_Universes/Resource.h
@@ -0,0 +1,64 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Across_Universes.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_ACROSSUNIVERSES_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_ACROSSUNIVERSES			107
+#define IDI_SMALL				108
+#define IDC_ACROSSUNIVERSES			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Следующие стандартные значения для новых объектов
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
+
+#pragma once
+
+// Modify the following defines if you have to target a platform prior to the ones specified below.
+// Refer to MSDN for the latest info on corresponding values for different platforms.
+#ifndef WINVER				// Allow use of features specific to Windows XP or later.
+#define WINVER 0x0501		// Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
+#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
+#endif						
+
+#ifndef _WIN32_WINDOWS		// Allow use of features specific to Windows 98 or later.
+#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
+#endif
+
+#ifndef _WIN32_IE			// Allow use of features specific to IE 6.0 or later.
+#define _WIN32_IE 0x0600	// Change this to the appropriate value to target other versions of IE.
+#endif
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <math.h>
+
+
+// TODO: reference additional headers your program requires here
